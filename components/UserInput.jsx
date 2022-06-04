211e686263fb6380e27699a8b82c2b0ebdff6a05
nut1414 <nut.pannapat@gmail.com>
2022-06-05 01:59:43 +0700

    form change

--- components/UserInput.jsx
+++ components/UserInput.jsx
@@ -7,7 +7,8 @@ const UserInput = () => {
   const tasks = useContext(TasksContext)
   const settasks = useContext(SetTasksContext)
   const nameref = useRef()
-  const addtasks = () => {
+  const addtasks = (e) => {
+    e.preventDefault()
     if (nameref.current?.value){
       const newTempTask = {_id: (new Date()).toISOString() ,name: nameref.current?.value, done:0}
       const newTempTasks = [...tasks,newTempTask]
@@ -19,10 +20,10 @@ const UserInput = () => {
   }
   return (
   <div className="flex flex-row justify-center justify-items-center items-center">
-    <form id="addform" action="#">
+    <form id="addform" action="/" onSubmit={addtasks}>
       <label className="self-center text-xl">Add Task: 
         <input ref={nameref} form="addform" required type="text" className="bg-neutral-50 drop-shadow-xl ring-2 ring-gray-100 rounded-full m-2 px-2 min-w-[50%] self-center focus:scale-[1.02] transition-all ease-in-out"></input>
-        <RoundButton form="addform" onClick={addtasks} rcolor='transparent'>
+        <RoundButton form="addform" rcolor='transparent'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
           </svg>
